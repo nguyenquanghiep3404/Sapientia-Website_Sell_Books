444c5f87f49a257cc6747ad12eae7a0c25f36d87
nguyenquanghiep3404 <nguyenquanghiep3404@gmail.com>
2025-03-29 13:37:57 +0700

    #6-day giao dien admin

--- views/admin/admin-broad.php
+++ views/admin/admin-broad.php
@@ -11,7 +11,7 @@
                               <div class="row">
                                    <div class="col-12">
                                         <div class="alert alert-primary text-truncate mb-3" role="alert">
-                                             ffffff
+                                             fffffffffff
                                         </div>
                                    </div>
 
