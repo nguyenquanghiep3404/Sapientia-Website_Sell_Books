bc45fbc452a48225e09b885da40b93f07772978c
nguyenquanghiep3404 <nguyenquanghiep3404@gmail.com>
2025-04-10 13:17:39 +0700

    fix loi giao dien

--- commons/env.php
+++ commons/env.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 
-define('BASE_URL'       , 'http://localhost/Du-an-1_Nhom5/');
-define('BASE_URL_ADMIN' , 'http://localhost/Du-an-1_Nhom5/admin/');
+define('BASE_URL', 'http://localhost/Sapientia-Website_Sell_Books/');
+define('BASE_URL_ADMIN', 'http://localhost/Sapientia-Website_Sell_Books/admin/');
 
-define('DB_HOST'    , 'localhost');
-define('DB_PORT'    , 3306);
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'duan_1');
+define('DB_NAME', 'duan_1');
 
-define('PATH_ROOT'    , __DIR__ . '/../');
+define('PATH_ROOT', __DIR__ . '/../');
